e98b170dba31806b3a8c97cdf9184ce762beef52
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-06-07 16:25:40 -0400

    Updated backend and added skeleton code for routes/views

--- server.js
+++ server.js
@@ -10,15 +10,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// GET /
 // Display form
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'public', index.html));
+  res.json({
+    'message': 'ayy'
+  });
 });
 
+// GET /dashboard
+// Show dashboard
+app.get('/dashboard', function(req, res) {
+  res.json({
+    'message': 'ayy'
+  });
+});
+
+// POST /api/alert/new
 // Send alert to headquarters
-app.post('/api/crew/alert', function(req, res) {
+app.post('/api/alert/new', function(req, res) {
   // Take data and store in database
-  // Send out e-mail
 });
 
 app.listen(3001, function() {
