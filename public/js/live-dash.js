2b896df63607d1b210f0e6af52ec8dd4e80ad270
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-06-07 23:08:40 -0400

    Implemented front end with back end, live dashboard

---
+++ public/js/live-dash.js
@@ -0,0 +1,43 @@
+var socket = io('/');
+
+socket.on('alert:new', function (data) {
+	var table = document.querySelector('.dash');
+
+	var row = document.createElement('tr');
+
+	var priority = document.createElement('td');
+
+	if (data.priority == 3) {
+		priority.innerHTML = "<span class=\"label label-danger\">High</span>";
+	} else if (data.priority == 2) {
+		priority.innerHTML = "<span class=\"label label-info\">Medium</span>";
+	} else {
+		priority.innerHTML = "<span class=\"label label-success\">Low</span>";
+	}
+
+	var issueType = document.createElement('td');
+	issueType.innerHTML = data.issueType;
+
+	var flightNumber = document.createElement('td');
+	flightNumber.innerHTML = data.flightNumber;
+
+	var comments = document.createElement('td');
+	comments.innerHTML = data.comments;
+
+	var status = document.createElement('td');
+	status.innerHTML = data.status;
+
+	var dateCol = document.createElement('td');
+	var date = new Date(data.created);
+	console.log(data.created);
+	dateCol.innerHTML = (date.getMonth()+1) + '/' + date.getDay() + ' ' + date.getHours() + ':' + date.getMinutes();
+
+	row.appendChild(priority);
+	row.appendChild(issueType);
+	row.appendChild(flightNumber);
+	row.appendChild(comments);
+	row.appendChild(status);
+	row.appendChild(dateCol)
+
+	table.appendChild(row);
+});
